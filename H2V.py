5ff22fa059a1df542cdac7139a74cb97b1d81a1a
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2019-12-08 03:09:35 +0800

    Add files via upload

--- H2V.py
+++ H2V.py
@@ -1,14 +1,34 @@
 #-*- encoding:utf-8 -*-
 import re
 
-def transfer(text):
-    str=[]
+def directTest(text):
+    str = []
     for i in text:
         str.append(i)
-        print(i)        
-    
-        
-text = input("Text:")
-transfer(text)
+        print(i)
 
+def transfer(text):
+    pattern = r',|\.|/|;|\'|`|\[|\]|<|>|\?|:|"|\{|\}|\~|!|@|#|\$|%|\^|&|\(|\)|-|=|\_|\+|，|？|。|、|；|‘|’|【|】|·|！| |…|（|）'
+    split = re.split(pattern,text)
+    split.reverse()
+    lens = len(split)
+    lines = len(max(split)) #len cannot get correct result for Chinese character 
+    print(lines)
+    space = []
+    for a in split:
+        d = lines - len(a)
+        a += ("　" *d)
+        space.append(a)
+    str = "".join(space)
+    s = ""
+    for i in range(0, lines):
+        for j in range(0, lens):
+            s += str[i + j*lines] + ""
+        s += "\n"
+    print(s)
+
+if __name__ == "__main__":        
+    #text = input("Text:")
+    text = "小陳同學，你好"
+    transfer(text)
 
