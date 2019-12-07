6451ecd8d72490dee17b5cb44e3e173da9ac5719
Harold Lee <31803859+zttofficial@users.noreply.github.com>
2019-12-07 23:19:58 +0800

    Add files via upload

---
+++ H2V.py
@@ -0,0 +1,14 @@
+#-*- encoding:utf-8 -*-
+import re
+
+def transfer(text):
+    str=[]
+    for i in text:
+        str.append(i)
+        print(i)        
+    
+        
+text = input("Text:")
+transfer(text)
+
+
